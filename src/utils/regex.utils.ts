9fb5b9d406b7f7cf1ebf795c227383f93e472833
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-03-31 04:01:25 +0900

    주석 수정정

---
+++ src/utils/regex.utils.ts
@@ -0,0 +1,8 @@
+//////////////////////
+//  정규표현식 정의  //
+//////////////////////
+
+//  숫자만 가능
+export const regNumberOnly = /^\d+$/
+//  기본 이메일
+export const regCommonEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
