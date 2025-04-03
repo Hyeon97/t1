298d365f9ffb61ef51018a1045c3d01fd99f2efc
Hsoh <dhdhgustlr@naver.com>
2025-04-03 15:31:17 +0900

    zdm 관련 에러 추가

--- src/errors/domain-errors/ZdmError.ts
+++ src/errors/domain-errors/ZdmError.ts
@@ -7,6 +7,59 @@ import { AppError } from "../AppError"
 
 export namespace ZdmError {
   /**
+   * ZDM 찾을 수 없음
+   */
+  export class ZdmNotFound extends AppError {
+    zdm: string | number
+    type: "id" | "name"
+
+    constructor({ zdm, type }: { zdm: string | number; type: "id" | "name" }) {
+      const message = type === "id" ? `ID가 ${zdm}인 ZDM을 찾을 수 없습니다` : `이름이 ${zdm}인 ZDM을 찾을 수 없습니다`
+      super({ message })
+      this.zdm = zdm
+      this.type = type
+    }
+    toApiError(): ApiError {
+      return ApiError.notFound({
+        message: this.message,
+        details: { zdm: this.zdm, type: this.type },
+      })
+    }
+  }
+
+  /**
+   * ZDM 요청 파라미터 에러
+   */
+  export class ZdmRequestParameterError extends AppError {
+    details: Record<string, any>
+    operation?: string
+    processingStage?: string
+    constructor({
+      details,
+      message,
+      processingStage,
+      operation,
+    }: {
+      details: Record<string, any>
+      message: string
+      processingStage?: string
+      operation?: string
+    }) {
+      super({ message })
+      this.details = details
+      this.processingStage = processingStage
+      this.operation = operation
+    }
+
+    toApiError(): ApiError {
+      return ApiError.badRequest({
+        message: this.message,
+        details: this.details,
+      })
+    }
+  }
+
+  /**
    * 데이터 처리 및 가공 실패
    */
   export class DataProcessingError extends AppError {
