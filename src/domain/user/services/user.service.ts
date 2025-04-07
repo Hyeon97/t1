d2274ec1fc55f0a521b801b8c493b51cf675fe78
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-04-08 00:10:02 +0900

    변경된 에러 객체에 맞춰 USER 관련 코드 수정

--- src/domain/user/services/user.service.ts
+++ src/domain/user/services/user.service.ts
@@ -1,12 +1,15 @@
-import { UserError } from "../../../errors/domain-errors/UserError"
-import { handleServiceError } from "../../../errors/handler/integration-error-handler"
+import { ServiceError } from "../../../errors/service/service-error"
+import { BaseService } from "../../../utils/base/base-service"
 import { ContextLogger } from "../../../utils/logger/logger.custom"
 import { UserInfoRepository } from "../repositories/user-info.repository"
 import { UserInfoTable } from "../types/db/user_info"
 
-export class UserService {
+export class UserService extends BaseService {
   private readonly userInfoRepository: UserInfoRepository
   constructor({ userInfoRepository }: { userInfoRepository: UserInfoRepository }) {
+    super({
+      serviceName: "UserService",
+    })
     this.userInfoRepository = userInfoRepository
   }
 
@@ -18,17 +21,17 @@ export class UserService {
       ContextLogger.debug({ message: `이메일 ${email}로 사용자 조회` })
       const user = await this.userInfoRepository.findByEmail({ email })
       if (!user) {
-        throw new UserError.UserNotFound({ user: email, type: "Email" })
+        throw ServiceError.resourceNotFoundError({
+          functionName: "getUserByEmail",
+          message: `Mail이 '${email}'인 User를 찾을 수 없습니다`,
+        })
       }
       return user
     } catch (error) {
-      return handleServiceError({
+      return this.handleServiceError({
         error,
-        logErrorMessage: "User 정보 조회중 UserService.getUserByEmail() 에러 발생",
-        apiErrorMessage: "User 정보 조회 중 오류가 발생했습니다",
-        operation: "User 정보 조회",
-        // processingStage: "조회",
-        errorCreator: (params) => new UserError.DataProcessingError(params),
+        functionName: "getUserByEmail",
+        message: `User 정보 조회 중 오류가 발생했습니다`,
       })
     }
   }
