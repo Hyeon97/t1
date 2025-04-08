031bb255536e5be0e97c57814b19935b2aa45ab0
Hsoh <dhdhgustlr@naver.com>
2025-04-08 16:56:38 +0900

    선언 수정 및 주석 수정

--- src/domain/zdm/services/service-registry.ts
+++ src/domain/zdm/services/service-registry.ts
@@ -2,16 +2,18 @@
 //  ZDM Service 선언 및 관리  //
 ///////////////////////////////
 
+import { ZdmZosRepositoryRepository } from "../repositories/center-zos-repository.repository"
 import { ZdmDiskRepository } from "./../repositories/center-info-disk.repository"
 import { ZdmNetworkRepository } from "./../repositories/center-info-network.repository"
 import { ZdmPartitionRepository } from "./../repositories/center-info-partition.repository"
 import { ZdmRepository } from "./../repositories/center-info.repository"
 import { ZdmRepositoryRepository } from "./../repositories/center-repository.repository"
-import { ZdmZosRepositoryRepository } from "../repositories/center-zos-repository.repository"
-import { ZdmService } from "./zdm.service"
 import { ZdmRepositoryService } from "./zdm.repository.service"
+import { ZdmService } from "./zdm.service"
 
-// 리포지토리 인스턴스 생성
+/**
+ * 리포지토리 인스턴스 생성
+ */
 const zdmRepository = new ZdmRepository()
 const zdmDiskRepository = new ZdmDiskRepository()
 const zdmNetworkRepository = new ZdmNetworkRepository()
@@ -19,7 +21,10 @@ const zdmPartitionRepository = new ZdmPartitionRepository()
 const zdmRepositoryRepository = new ZdmRepositoryRepository()
 const zdmZosRepositoryRepository = new ZdmZosRepositoryRepository()
 
-// 서비스 인스턴스 생성 및 의존성 주입
+/**
+ * 서비스 인스턴스 생성 및 의존성 주입
+ */
+//  ZDM 정보 조회 Service
 export const zdmService = new ZdmService({
   zdmRepository,
   zdmDiskRepository,
@@ -28,7 +33,7 @@ export const zdmService = new ZdmService({
   zdmRepositoryRepository,
   zdmZosRepositoryRepository,
 })
-
+///  ZDM Repository 정보 조회 Service
 export const zdmRepositoryService = new ZdmRepositoryService({
   zdmRepository,
   zdmRepositoryRepository,
