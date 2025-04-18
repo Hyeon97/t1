d3c1c25be1c182e4fa5089f5b516495b33ed5758
Hsoh <dhdhgustlr@naver.com>
2025-04-18 17:07:24 +0900

    backup monitoring 작업 이름으로 조회 추가 시작.

--- src/domain/backup/controllers/monitoring/backup.monitoring.get.controller.ts
+++ src/domain/backup/controllers/monitoring/backup.monitoring.get.controller.ts
@@ -1,7 +1,13 @@
+import { NextFunction, Response } from "express"
 import { ControllerError } from "../../../../errors"
+import { ExtendedRequest } from "../../../../types/common/req.types"
+import { ApiUtils } from "../../../../utils/api/api.utils"
 import { asyncContextStorage } from "../../../../utils/AsyncContext"
 import { BaseController } from "../../../../utils/base/base-controller"
+import { stringToBoolean } from "../../../../utils/data-convert.utils"
+import { ContextLogger } from "../../../../utils/logger/logger.custom"
 import { BackupGetQueryDTO } from "../../dto/query/backup-get-query.dto"
+import { BackupResponseFactory } from "../../dto/response/backup-response-factory"
 import { BackupRegistService } from "../../services/backup-regist.service"
 import { BackupService } from "../../services/backup.service"
 import { BackupFilterOptions } from "../../types/backup-get.type"
@@ -44,6 +50,41 @@ export class BackupMonitoringController extends BaseController {
         method: "extractFilterOptions",
         message: "[Backup 조회 필터 옵션 추출] - 필터 옵션 확인필요",
         error,
+      })
+    }
+  }
+
+  /**
+   * Backup 작업 이름으로 모니터링
+   */
+  monitByJobName = async (req: ExtendedRequest, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      ContextLogger.debug({ message: `Backup 작업 이름으로 모니터링 시작` })
+      asyncContextStorage.setController({ name: this.controllerName })
+      asyncContextStorage.addOrder({ component: this.controllerName, method: "monitByJobName", state: "start" })
+
+      // 필터 옵션 추출
+      const query = req.query as unknown as BackupGetQueryDTO
+      const filterOptions = this.extractFilterOptions({ query })
+      ContextLogger.debug({ message: `적용된 필터 옵션`, meta: filterOptions })
+
+      //  서비스 호출
+      const backupsData = await this.backupService.getBackups({ filterOptions })
+
+      //  출력 가공
+      const backupsDTO = BackupResponseFactory.createFromEntities({
+        detail: stringToBoolean({ value: filterOptions?.detail }),
+        backupsData,
+      })
+      ContextLogger.info({ message: `총 ${backupsData.length}개의 Backup 작업 정보를 조회했습니다. 상세 정보 포함: ${filterOptions.detail}` })
+      ApiUtils.success({ res, data: backupsDTO, message: "Backup infomation list" })
+      asyncContextStorage.addOrder({ component: this.controllerName, method: "monitByJobName", state: "end" })
+    } catch (error) {
+      this.handleControllerError({
+        next,
+        error,
+        method: "getBackups",
+        message: "[Backup 작업 목록 조회] - 예기치 못한 오류 발생",
       })
     }
   }
