53ee493922261530004a85fed91f6e4670671aff
Hyeon97 <61956880+Hyeon97@users.noreply.github.com>
2025-04-28 03:40:26 +0900

    ScheduleDBRegistOnce interface 추가 및 기존 interface 값 수정.

--- src/domain/schedule/types/schedule-regist.type.ts
+++ src/domain/schedule/types/schedule-regist.type.ts
@@ -2,6 +2,16 @@
 //  Schedule 정보 등록 관련 인터페이스 정의  //
 ///////////////////////////////////////////
 
+import { ScheduleStatusEnum, ScheduleTypeEnum } from "./schedule-common.type"
+
+/**
+ * full, increment 내 interval 양식 정의
+ */
+export interface ScheduleDetailInterval {
+  hour: number
+  minute: number
+}
+
 /**
  * full, increment 양식 정의
  */
@@ -11,10 +21,7 @@ export interface ScheduleDetail {
   week: string
   day: string
   time: string
-  interval: {
-    minute: string
-    hour: string
-  }
+  interval: ScheduleDetailInterval
 }
 
 /**
@@ -24,7 +31,24 @@ export interface ScheduleRegistRequestBody {
   //  기본 정보
   center: string //  center 이름 or ID
   //  Schedule 관련 정보
-  type: string
+  type: ScheduleTypeEnum // 0 ~ 11 까지
   full?: ScheduleDetail
   increment?: ScheduleDetail
+}
+
+/**
+ * Schedule DB 등록 타입 - Once
+ */
+export interface ScheduleDBRegistOnce {
+  nID: number
+  nUserID: number
+  nCenterID: number
+  nScheduleType: ScheduleTypeEnum
+  nStatus: ScheduleStatusEnum
+  sYear: string
+  sMonth: string
+  sDay: string
+  sTime: string
+  nOffset: number
+  sJobName: string
 }
