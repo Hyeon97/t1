206b3bc0bc99e88fccab3faed31e89d34502ebb8
Hsoh <dhdhgustlr@naver.com>
2025-05-14 16:59:01 +0900

    server 관련 엔드포인트 수정.

--- src/domain/server/routes/server.routes.ts
+++ src/domain/server/routes/server.routes.ts
@@ -1,6 +1,10 @@
 import { Router } from "express"
 import { serverGetController } from "../controllers/controller-registry"
-import { validateServerListQuery, validateSpecificServerParams, validateSpecificServerQuery } from "../validators/server.validators"
+import {
+  validateServerGetByServerIdParams,
+  validateServerGetByServerNameParams,
+  validateServerGetQuery
+} from "../validators/server.validators"
 
 export class ServerRoutes {
   public router: Router
@@ -11,11 +15,19 @@ export class ServerRoutes {
   }
 
   private serverRoutes(): void {
+    //  [Server 정보 조회]
     //  전체 목록 리턴
-    this.router.get("/", validateServerListQuery, serverGetController.getServers)
-    //  특정 조건으로 조회 ( 기준 : server name | server id )
-    this.router.get("/:identifier", validateSpecificServerParams, validateSpecificServerQuery, serverGetController.getServer)
+    this.router.get("/", validateServerGetQuery, serverGetController.getServers)
+    //  Server 이름으로 조회
+    this.router.get('/server-name/:serverName', validateServerGetByServerNameParams, validateServerGetQuery, serverGetController.getServerByName)
+    //  Server ID로 조회
+    this.router.get('/server-id/:serverId', validateServerGetByServerIdParams, validateServerGetQuery, serverGetController.getServerById)
+    //  [Server 정보 삭제]
     //  특정 조건으로 삭제 ( 기준 : server name | server id )
     this.router.delete("/:identifier")
+    //  Server 이름으로 삭제
+    this.router.delete('/server-name/:serverName')
+    //  Server ID로 삭제
+    this.router.delete('/server-id/:serverID')
   }
 }
