0f781d0d52b23e5092ce5cdc5a14bb3419fc8ff6
Hsoh <dhdhgustlr@naver.com>
2025-05-16 16:15:50 +0900

    center 타입 string | number -> string 으로 변경, @Expose() 데코레이터 적용.

--- src/domain/zdm/dto/query/zdm-repository/zdm-repository-query-filter.dto.ts
+++ src/domain/zdm/dto/query/zdm-repository/zdm-repository-query-filter.dto.ts
@@ -2,28 +2,34 @@
 //  zdm repository data 조회 공통 필터링 옵션 DTO  //
 ///////////////////////////////////////////////////
 
-import { IsOptional, IsIn } from "class-validator"
+import { Expose } from "class-transformer"
+import { IsIn, IsOptional } from "class-validator"
 import { OSType } from "../../../../../types/common/os"
 import { RepositoryType } from "../../../../../types/common/repository"
 import { ZDMSearchType } from "../../../types/zdm-common.type"
 import { ZdmRepositoryFilterOptions } from "../../../types/zdm-repository/zdm-repository-filter.type"
 
-export class ZdmRepositoryFilterDTO implements ZdmRepositoryFilterOptions {
+export class ZdmRepositoryGetQueryDTO implements ZdmRepositoryFilterOptions {
   //  ZDM id | name
   @IsOptional()
   @IsIn(["name", "id"], { message: 'identifierType는 필수이며 값은 "name" 또는 "id"만 가능합니다' })
+  @Expose()
   identifierType?: ZDMSearchType
 
   @IsOptional()
-  center?: string | number
+  @Expose()
+  center?: string
 
   @IsOptional()
+  @Expose()
   type?: RepositoryType | ""
 
   @IsOptional()
+  @Expose()
   os?: OSType | ""
 
   //  repository sRemotePath
   @IsOptional()
+  @Expose()
   path?: string
 }
