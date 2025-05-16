fef4b5ee141e18d37de9a528bddb10ab1d3719bd
Hsoh <dhdhgustlr@naver.com>
2025-05-16 16:21:52 +0900

    @Expose() 데코레이터 추가. bool 관련 변수들 검증에 false 추가, 에러 메시지 수정.

--- src/domain/zdm/dto/query/zdm/zdm-query-filter.dto.ts
+++ src/domain/zdm/dto/query/zdm/zdm-query-filter.dto.ts
@@ -2,43 +2,52 @@
 //  zdm data 조회 공통 필터링 옵션 DTO  //
 ////////////////////////////////////////
 
+import { Expose } from "class-transformer"
 import { IsIn, IsOptional } from "class-validator"
 import { ZDMActivationType, ZDMConnectType } from "../../../types/zdm-common.type"
 import { ZdmFilterOptions } from "../../../types/zdm/zdm-filter.type"
 
-export class ZdmQueryFilterDTO implements ZdmFilterOptions {
+export class ZdmGetQueryDTO implements ZdmFilterOptions {
   //  일반 필터링
   @IsOptional()
   @IsIn(["connect", "disconnect"], { message: "connection은 'connect' 또는 'disconnect'만 가능합니다" })
+  @Expose()
   connection?: ZDMConnectType
 
   @IsOptional()
   @IsIn(["ok", "fail"], { message: "activation은 'ok' 또는 'fail'만 가능합니다" })
+  @Expose()
   activation?: ZDMActivationType
 
   //  추가 정보
   @IsOptional()
-  @IsIn(["true"], { message: "network는 'true'만 가능합니다" })
+  @IsIn(["true", "false"], { message: "network는 'true','false'만 가능합니다" })
+  @Expose()
   network?: string
 
   @IsOptional()
-  @IsIn(["true"], { message: "disk는 'true'만 가능합니다" })
+  @IsIn(["true", "false"], { message: "disk는 'true','false'만 가능합니다" })
+  @Expose()
   disk?: string
 
   @IsOptional()
-  @IsIn(["true"], { message: "partition은 'true'만 가능합니다" })
+  @IsIn(["true", "false"], { message: "partition은 'true','false'만 가능합니다" })
+  @Expose()
   partition?: string
 
   @IsOptional()
-  @IsIn(["true"], { message: "repository는 'true'만 가능합니다" })
+  @IsIn(["true", "false"], { message: "repository는 'true','false'만 가능합니다" })
+  @Expose()
   repository?: string
 
   @IsOptional()
-  @IsIn(["true"], { message: "zosRepository는 'true'만 가능합니다" })
+  @IsIn(["true", "false"], { message: "zosRepository는 'true','false'만 가능합니다" })
+  @Expose()
   zosRepository?: string
 
   //  상세 정보 추가
   @IsOptional()
   @IsIn(["true", "false"], { message: "detail은 'true','false'만 가능합니다" })
+  @Expose()
   detail?: string
 }
