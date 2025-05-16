9ed4c335f08ee2d4f0f28555123b4266823cab1a
Hsoh <dhdhgustlr@naver.com>
2025-05-16 16:19:04 +0900

    zdmRepositoryGetController Controller 추가.

--- src/domain/zdm/controllers/controller-registry.ts
+++ src/domain/zdm/controllers/controller-registry.ts
@@ -2,11 +2,14 @@
 //  ZDM Controller 선언 및 관리  //
 //////////////////////////////////
 
-import { zdmGetService } from "../services/service-registry"
+import { zdmGetService, zdmRepositoryGetService } from "../services/service-registry"
 import { ZdmGetController } from "./common/zdm-get.controller"
+import { ZdmRepositoryGetController } from "./repository/zdm.repository-get.controller"
 
 /**
  * ZDM 컨트롤러 인스턴스 생성 및 의존성 주입
  */
 //  ZDM 정보 조회
 export const zdmGetController = new ZdmGetController({ zdmGetService })
+//  ZDM Repository 정보 조회
+export const zdmRepositoryGetController = new ZdmRepositoryGetController({ zdmRepositoryGetService })
