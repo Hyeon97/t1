ade690b7d9a418b8df57b66a092d2c653aa971a2
Hsoh <dhdhgustlr@naver.com>
2025-05-16 16:12:10 +0900

    엔드포인트 수정.

--- src/domain/zdm/routes/zdm.routes.ts
+++ src/domain/zdm/routes/zdm.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express"
-import { zdmGetController } from "../controllers/controller-registry"
-import { validateSpecificZdmParams, validateSpecificZdmQuery, validateZdmListQuery } from "../validators/zdm"
+import { zdmGetController, zdmRepositoryGetController } from "../controllers/controller-registry"
+import { validateRepositoryGetQuery, validateZdmGetByZdmIdParamas, validateZdmGetByZdmNameParams, validateZdmGetQuery } from "../validators/zdm"
 
 export class ZdmRoutes {
   public router: Router
@@ -14,12 +14,18 @@ export class ZdmRoutes {
 
   //  ZDM 관련
   private zdmRoutes(): void {
+    //  [ZDM 정보 조회]
     //  전체 목록 리턴
-    this.router.get("/", validateZdmListQuery, zdmGetController.getZdms)
-    //  특정 조건으로 조회 ( 기준 : ZDM name | ZDM id )
-    this.router.get("/:identifier", validateSpecificZdmParams, validateSpecificZdmQuery, zdmGetController.getZdm)
-    //  특정 조건으로 삭제 ( 기준 : ZDM name | ZDM id )
-    this.router.delete("/:identifier")
+    this.router.get("/", validateZdmGetQuery, zdmGetController.getZdms)
+    //  ZDM 이름으로 조회
+    this.router.get("/zdm-name/:zdmName", validateZdmGetByZdmNameParams, validateZdmGetQuery, zdmGetController.getZdmByName)
+    //  ZDM ID로 조회
+    this.router.get("/zdm-id/:zdmId", validateZdmGetByZdmIdParamas, validateZdmGetQuery, zdmGetController.getZdmById)
+    //  [ZDM 정보 삭제]
+    //  ZDM 이름으로 삭제
+    this.router.delete("/zdm-name/:zdmName")
+    //  ZDM ID로 삭제
+    this.router.delete("/zdm-id/:zdmId")
   }
 
   //  ZDM repository 관련
@@ -27,7 +33,7 @@ export class ZdmRoutes {
     // //  ZDM 레포지토리 정보 추가
     // this.router.post("/:identifier/repositories")
     // //  ZDM 전체 레포지토리 조회
-    // this.router.get("/repositories", validateRepositoryListQuery, zdmRepositoryController.getRepositories)
+    this.router.get("/repositories", validateRepositoryGetQuery, zdmRepositoryGetController.getRepositories)
     // //  특정 ZDM의 전체 레포지토리 조회 ( 기준 : zdm id | zdm name )
     // this.router.get("/:identifier/repositories")
     // //  특정 ZDM의 특정 레포지토리 조회 ( 기준 : zdm id | zdm name, repository id )
