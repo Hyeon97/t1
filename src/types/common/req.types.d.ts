f1a4e2ddade2b3092ec2e6e7e22b467977b21580
Hsoh <dhdhgustlr@naver.com>
2025-05-22 17:02:21 +0900

    인터페이스 변경.

--- src/types/common/req.types.d.ts
+++ src/types/common/req.types.d.ts
@@ -2,8 +2,10 @@ import { Request } from "express"
 
 // Express 요청 객체 확장
 export interface ExtendedRequest extends Request {
-  user?: {
-    id: number
-    email: string
-  }
+  user?: AuthenticatedUser
+}
+//  Express 요청 객체 확장 - User ( 정상 적으로 토큰이 검증된 경우 무조건 존재함 )
+export interface AuthenticatedUser {
+  id: number
+  email: string
 }
